3c7b2934a22d17227bd04d8e5b4cfd1c15b8d369
jarrett <jarrettpdunne@gmail.com>
2021-04-02 14:07:13 -0500

    update controllers/users.js

---
+++ controllers/users.js
@@ -0,0 +1,72 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const db = require('../db/connection')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const SALT_ROUNDS = 11
+const TOKEN_KEY = 'wowthisisastrongkey'
+
+const signUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS)
+    const user = new User({
+      username,
+      email,
+      password_digest
+    })
+
+    await user.save()
+
+    const payload = {
+      username: user.username,
+      email: user.email
+    }
+
+    const token = jwt.sign(payload, TOKEN_KEY)
+    res.status(201).json({ token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const signIn = async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const user = await User.findOne({ username: username })
+    if (await brcypt.compare(password, user.password_digest)) {
+      const payload = {
+        username: user.username,
+        email: user.email
+      }
+
+      const token = jwt.sign(payload, TOKEN_KEY)
+      res.status(201).json({ token })
+    } else {
+      res.status(500).json({ error: error.message })
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const verify = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    const payload = jwt.verify(token, TOKEN_KEY)
+
+    if (payload) {
+      res.json(payload)
+    }
+  } catch (error) {
+    res.status(401).send('Not Austhorized')
+  }
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  verify
+}
