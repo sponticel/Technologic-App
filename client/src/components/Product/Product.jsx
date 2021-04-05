b7a2b630a891247ac7f2a5d828bcf4fa5808ace4
jcameron10 <jovonnec@gmail.com>
2021-04-05 15:56:37 -0400

    product edit screen

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -6,7 +6,7 @@ const Product = (props) => {
     return (
         <>
             <Link className="product" to={`/products/${props._id}`}>
-                <img className="product-image" src={props.imgURL} alt={props.name} />
+                <img className="product-image" src={props.imgURL1} alt={props.name} />
                 {/* <img className="product-image" src={props.imgURL2} alt={props.name} />
                 <img className="product-image" src={props.imgURL3} alt={props.name} />                 */}
                 <div className="product-name">{props.name}</div>
