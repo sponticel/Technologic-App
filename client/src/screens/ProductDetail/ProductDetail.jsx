a874afce9a9e931b193c8752f325f26414bbe04c
jcameron10 <jovonnec@gmail.com>
2021-04-08 12:53:16 -0400

    conditionally rendering edit and delete on product detail

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -4,14 +4,14 @@ import Layout from "../../components/shared/Layout/Layout";
 import { getProduct, deleteProduct } from "../../services/products";
 import { useParams, Link, useHistory } from "react-router-dom";
 
-import Carousel from '../../components/shared/Carousel/Carousel'
+import Carousel from "../../components/shared/Carousel/Carousel";
 
 const ProductDetail = (props) => {
   const [product, setProduct] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
   const { id } = useParams();
 
-  const history = useHistory()
+  const history = useHistory();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -27,31 +27,33 @@ const ProductDetail = (props) => {
   }
 
   const delProd = () => {
-    deleteProduct(product._id)
-    history.push('/products')
-  }
-  
+    deleteProduct(product._id);
+    history.push("/products");
+  };
+
   return (
     <Layout user={props.user}>
       <div className="product-detail">
-        <Carousel images={product.images}/>
+        <Carousel images={product.images} />
         <div className="detail">
           <div className="name">{product.name}</div>
           <div className="condition">{product.condition}</div>
           <div className="price">{`$${product.price}`}</div>
           <div className="details">{product.details}</div>
           <div className="button-container">
-            <Link className="edit-link" to={`/products/${product._id}/edit`}>
-              <button className="edit-button">
-              Edit
-              </button>
-            </Link>
-            <button
-              className="delete-button"
-              onClick={delProd}
-            >
-              Delete
-            </button>
+            {props.user && (
+              <>
+                <Link
+                  className="edit-link"
+                  to={`/products/${product._id}/edit`}
+                >
+                  <button className="edit-button">Edit</button>
+                </Link>
+                <button className="delete-button" onClick={delProd}>
+                  Delete
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
