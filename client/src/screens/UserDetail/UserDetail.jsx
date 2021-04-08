ccdb529cb2a9bb4be55a187dd6f1d77f1ffde71a
jarrett <jarrettpdunne@gmail.com>
2021-04-08 16:22:08 -0500

    Add products associated to the currently signed in user to the screen

--- client/src/screens/UserDetail/UserDetail.jsx
+++ client/src/screens/UserDetail/UserDetail.jsx
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
 
-import Layout from "../../components/shared/Layout/Layout"
+import { getUsers, getUserProducts } from "../../services/users";
+
+import Layout from "../../components/shared/Layout/Layout";
+import Product from "../../components/Product/Product";
 
 export default class UserDetail extends Component {
   constructor(props) {
     super(props);
-    this.user = props.user
+    this.user = props.user;
+    this.state = {
+      queriedProducts: [],
+      users: [],
+      userId: props.user.id,
+    };
+  }
+  
+  componentDidMount() {
+    const fetchUsers = async () => {
+      const userProducts = await getUserProducts(this.state.userId);
+      this.setState({
+        queriedProducts: userProducts,
+      });
+    };
+
+    fetchUsers();
+    this.setState({
+      users: [this.user],
+    });
   }
 
   profile = (user) => (
@@ -14,13 +35,28 @@ export default class UserDetail extends Component {
       <h1>{`Hello, ${user.username}`}</h1>
       <h3>{`${user.email}`}</h3>
     </>
-  )
-  
+  );
+
+  productsJSX() {
+    return this.state.queriedProducts.map((product, index) => (
+      <Product
+        _id={product._id}
+        name={product.name}
+        images={product.images}
+        price={product.price}
+        key={index}
+      />
+    ));
+  }
+
   render() {
     return (
       <>
         <Layout user={this.user}>
-          {this.user ? this.profile(this.user) : 'Maybe create an account.'}
+          {this.user ? this.profile(this.user) : "Maybe create an account."}
+          <div className="products">
+            {this.productsJSX()}
+          </div>
         </Layout>
       </>
     );
