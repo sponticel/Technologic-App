10febf06790f367bec93b0029bc2f43183e9ae52
jarrett <jarrettpdunne@gmail.com>
2021-04-08 21:16:43 -0500

    Remove Layout from imports since unused

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
-import Layout from "../../components/shared/Layout/Layout";
-
 import "./Home.css";
 
 const Home = (props) => {
