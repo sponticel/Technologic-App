a0baac70076cd87f0cfeddbeda36ee8ddacf237f
jarrett <jarrettpdunne@gmail.com>
2021-04-08 21:14:46 -0500

    Filter images array to remove empty img links

--- client/src/components/shared/Carousel/Carousel.jsx
+++ client/src/components/shared/Carousel/Carousel.jsx
@@ -1,33 +1,32 @@
 import React from "react";
 import {
   CarouselProvider,
-  Dot,   
-  // DotGroup,
+  Dot,
   Slider,
   Slide,
   ButtonBack,
   ButtonNext,
-  DotGroup,
 } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
-import "./Carousel.css"
+import "./Carousel.css";
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.images = props.images;
+    this.images = props.images.filter((i) => i);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.totalSlides = this.images;
+  }
 
   componentWillUnmount() {}
 
   render() {
-    let key = 0
     return (
       <CarouselProvider
-        naturalSlideWidth={100  }
-        naturalSlideHeight={125}  
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
         totalSlides={this.images.length}
         dragEnabled={false}
         infinite={true}
@@ -36,23 +35,21 @@ class Carousel extends React.Component {
       >
         <div className="carousel-main">
           <Slider>
-            {this.images.map((i) => (
-              
-                <Slide key={`carousel-img-key-${key++}`} index={key}>
-                  <img id="myImg" src={i} alt="" />
-                </Slide>
-              
+            {this.images.map((v, i) => (
+              <Slide key={i}>
+                <img id="myImg" src={v} alt="" />
+              </Slide>
             ))}
           </Slider>
           <div className="carousel-buttons">
-            <ButtonBack>{'<'}</ButtonBack>
-            <ButtonNext>{'>'}</ButtonNext>
+            <ButtonBack>{"<"}</ButtonBack>
+            <ButtonNext>{">"}</ButtonNext>
           </div>
         </div>
         <div className="carousel-dots">
           {this.images.map((v, i) => (
-            <Dot key={i} slide={i} >
-              <img className="carousel-dot-img" src={v} alt=""/>
+            <Dot key={i} slide={i}>
+              <img className="carousel-dot-img" src={v} alt="" />
             </Dot>
           ))}
         </div>
@@ -61,4 +58,4 @@ class Carousel extends React.Component {
   }
 }
 
-export default Carousel
+export default Carousel;
