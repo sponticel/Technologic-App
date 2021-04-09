e84bbbefe1a5e56bb1a0f0fd763671622fe9cf0c
jarrett <jarrettpdunne@gmail.com>
2021-04-08 21:15:35 -0500

    Remove Redirect from imported package functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { verifyUser } from "./services/users";
 
 import Home from "./screens/Home/Home";
