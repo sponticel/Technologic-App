d3fa38139572bf04db2369c27d745cfc0859397f
jcameron10 <jovonnec@gmail.com>
2021-04-09 11:47:41 -0400

    hamburger menu

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -3,7 +3,6 @@ import "./Nav.css";
 import { NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-
 const authenticatedOptions = (
   <>
     <NavLink className="link" to="/add-product">
@@ -38,45 +37,50 @@ const alwaysOptions = (
 );
 
 const Nav = ({ user }) => {
-  const [hamburger, setHamburger] = useState(true);
+  const [hamburgerOptions, setHamburgerOptions] = useState(true);
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
+    //called when component mounts (opens/on the path or mounted on the screen) or when something in depency array changes
+
     const handleResize = (e) => {
       if (window.innerWidth > 425) {
-        setHamburger(true)
-        setVisible(true)
+        setHamburgerOptions(true);
+        setVisible(true);
       } else if (window.innerWidth <= 425) {
-        setHamburger(false)
+        setHamburgerOptions(false);
       }
-    }
-    window.addEventListener('resize', handleResize)
+    };
+    handleResize(); //checks to see the size at the point oof mounting
+    window.addEventListener("resize", handleResize); //also runs when window is resized
     return () => {
-      window.removeEventListener('resize', handleResize)
-
-    }
-  },[])
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  //when the screen is > 425 show me the options
+  // when < 425 conditionally render (js) the options
 
   return (
     <nav>
-        <NavLink className="logo" to="/">
-          Technologic
-        </NavLink>
+      <NavLink className="logo" to="/">
+        Technologic
+      </NavLink>
       <div className="nav">
-        <img
+        <img //rendered via a media query on css
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png"
           className="bars"
           alt="hamburger"
-          onClick={() => setHamburger(!hamburger)}
+          onClick={() => setHamburgerOptions(!hamburgerOptions)}
         />
-        <div className="links"
-          style={{ display: hamburger && visible ? "flex" : "none" }}>
+        <div
+          className="links"
+          style={{ display: hamburgerOptions && visible ? "flex" : "none" }}
+        >
           {/* {user && <div className="link welcome">Welcome, {user.username}</div>} */}
           {alwaysOptions}
           {user ? authenticatedOptions : unauthenticatedOptions}
-          </div>
         </div>
-      
+      </div>
     </nav>
   );
 };
